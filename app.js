2aeb41837e64069ba9c2492dc4b32fc0e2c1366f
taddeimania <jtaddei@gmail.com>
2017-07-13 11:07:02 -0400

    make heroku work

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 const app = express();
 app.engine('mustache', mustacheExpress());
@@ -9,15 +10,27 @@ app.set('views', './views');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(expressValidator());
 
 app.get("/", (req, res) => {
   res.render("index", {});
 });
 
-app.post("/", (req, res) => {
-  let context = {}
+app.post("/signup", (req, res) => {
+  req.checkBody("first_name", "Field must not be empty.").notEmpty();
+  req.checkBody("last_name", "Field must not be empty.").notEmpty();
+  let errors = req.validationErrors();
+  let context = {};
+
+  if (errors) {
+    context["errors"] = errors;
+  }
   context["first_name"] = req.body.first_name;
+  context["last_name"] = req.body.last_name;
+
   res.render("index", context);
+
+
 });
 
 app.listen(3000);
